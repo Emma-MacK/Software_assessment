82414de4d73203235f346a4cac39afb94139ef6a
raymondmiles <raymond.c.miles@gmail.com>
2023-11-07 15:51:00 +0000

    Panel Source Argument Added

--- src/tool.py
+++ src/tool.py
@@ -2,8 +2,13 @@ import argparse
 # read commandline inputs/user
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-ID", "--testID", help="input the Test ID")
+argParser.add_argument("-PanS", "--PanelSource", help="input the Test ID")
+
 args = argParser.parse_args()
 testID = args.testID
+PanelSource = args.PanelSource
+
+
 if testID[:1] != "R":
     print("invalid R code")
 
