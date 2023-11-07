d4683a0344cde13697ce8cf09326c49741f47d01
raymondmiles <raymond.c.miles@gmail.com>
2023-11-07 15:19:02 +0000

    An argument parser takes testID. With requirement.txt and argument_parser class

--- src/tool.py
+++ src/tool.py
@@ -1,7 +1,11 @@
-
+import argparse
 # read commandline inputs/user
-
-
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-ID", "--testID", help="input the Test ID")
+args = argParser.parse_args()
+testID = args.testID
+if testID[:1] != "R":
+    print("invalid R code")
 
 
 # get info from test directory 
