68d0dd8e73a911250d6ce19621a7c3e630951af8
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2023-11-09 15:26:56 +0000

    create a bed file from json

--- Draft_scripts/emma_get_bed.py
+++ Draft_scripts/emma_get_bed.py
@@ -1,16 +1,50 @@
 # script for me to get bed file
 import requests
-
+import json
+import pandas
 
 # get HGNC id from JSON, API variant validator genes to transcripts, get bed files?
 # ie "HGNC:4562"
 # want the HGNC number
 # Merged into one bed for multiple gene beds?
 
+#try:
+#  r = requests.get("https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A4562/mane_select/refseq/GRCh37", headers={ "content-type" : "application/json"})
+#  decoded = r.json()
+#  print(repr(decoded))
+#except:
+#  print("An exception occurred")
+
+# json is actually in a list?
+with open('Draft_scripts/test.json') as f:
+    json_dict = json.load(f)[0]
+
+# Keys in json ['current_name', 'current_symbol', 'hgnc', 'previous_symbol', 'requested_symbol', 'transcripts']
+
+transcripts_list = json_dict["transcripts"]
+transcripts_dict= transcripts_list[0]
+
+# keys in subsection ['annotations', 'coding_end', 'coding_start', 'description', 'genomic_spans', 'length', 'reference', 'translation']
+# get the Refseq ID for database
+RefSeq = transcripts_dict["reference"]
 
 
-r = requests.get("https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A4562/mane_select/RefSeq/all", headers={ "content-type" : "application/json"})
+# bedfile header
+with open('output.bed', 'w') as f:
+    f.write("Chromosome\tstart\tend\tname\n")
 
-#returns data
-decoded = r.json()
-print(repr(decoded))
+
+# get chromosome for BED
+annotations_dict = transcripts_dict["annotations"]
+chromosome = str(annotations_dict["chromosome"])
+
+# get start and end position for BED for each transcript
+genomic_spans_dict = transcripts_dict["genomic_spans"]
+for key in genomic_spans_dict:
+    temp_dict = genomic_spans_dict[key]
+    start = temp_dict["start_position"]
+    end = temp_dict["end_position"]
+
+    # for each transcript reference, add to bed file
+    with open('output.bed', 'a') as f:
+        f.write(chromosome + "\t" + str(start) + "\t" + str(end) + "\t" + str(key) + "\n")
