83398dc4ef81c75795a36629536dd05a8dff2979
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2023-11-09 16:56:42 +0000

    turn script into funtion to loop over list of HGNC inputs

--- Draft_scripts/emma_get_bed.py
+++ Draft_scripts/emma_get_bed.py
@@ -3,63 +3,60 @@ import requests
 import json
 import pandas
 
-# get HGNC id from JSON, API variant validator genes to transcripts, get bed files?
 # ie "HGNC:4562"
 # want the HGNC number
-# Merged into one bed for multiple gene beds?
 
+def call_transcript_make_bed(HGNC_list):
+    url_base = "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A"
+    transcript_filter = "/mane_select/refseq/GRCh37"
+    for HGNC in HGNC_list:
 
-url = "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A"
+        full_url = url_base + str(HGNC)+ transcript_filter
+        print("querying: " + full_url)
+        try:
+            r = requests.get(full_url, headers={ "content-type" : "application/json"})
+            decoded = r.json()
+            # print(repr(decoded))
+            json_dict = decoded[0]
+        except:
+            print("An exception occurred connecting to variant validator")
 
+        # Keys in json ['current_name', 'current_symbol', 'hgnc', 'previous_symbol', 'requested_symbol', 'transcripts']
+        print("JSON found")
+        transcripts_list = json_dict["transcripts"]
+        transcripts_dict= transcripts_list[0]
 
-# TO DO
-HGNC = "4562"
+        # keys in subsection ['annotations', 'coding_end', 'coding_start', 'description', 'genomic_spans', 'length', 'reference', 'translation']
 
-# can personalise filtering later?
-transcript_filter = "/mane_select/refseq/GRCh37"
+        # make bedfile header
+        print("Making bed file for HGNC:" + str(HGNC))
+        filename = HGNC + "_output.bed"
+        with open(filename, 'w') as f:
+            f.write("Chromosome\tstart\tend\tname\texon\n")
 
-# full working url
-# "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A4562/mane_select/refseq/GRCh37"
-# build full request
+        # get chromosome for BED
+        annotations_dict = transcripts_dict["annotations"]
+        chromosome = str(annotations_dict["chromosome"])
 
-full_url = url + HGNC + transcript_filter
-print(full_url)
-try:
-   r = requests.get(full_url, headers={ "content-type" : "application/json"})
-   decoded = r.json()
-   print(repr(decoded))
-   json_dict = decoded[0]
-except:
-    print("An exception occurred")
-    with open('Draft_scripts/test.json') as f:
-        json_dict = json.load(f)[0]
+        # get the info for database
+        RefSeq = transcripts_dict["reference"]
+        ensembl_select = str(annotations_dict["ensembl_select"])
+        mane_plus_clinical = str(annotations_dict["mane_plus_clinical"])
+        mane_select = str(annotations_dict["mane_select"])
 
-# json is actually in a list?
+        # get start and end position for BED for each transcript
+        genomic_spans_dict = transcripts_dict["genomic_spans"]
+        for key in genomic_spans_dict:
+            temp_dict = genomic_spans_dict[key]
+            exon_list = temp_dict["exon_structure"]
+            for item in exon_list:
+                start = item["genomic_start"]
+                end = item["genomic_end"]
+                exon = item["exon_number"]
+                # for each transcript reference, add to bed file
+                with open(filename, 'a') as f:
+                    f.write(chromosome + "\t" + str(start) + "\t" + str(end) + "\t" + str(key) + "\t" + "exon_" +str(exon) + "\n")
 
-# Keys in json ['current_name', 'current_symbol', 'hgnc', 'previous_symbol', 'requested_symbol', 'transcripts']
-
-transcripts_list = json_dict["transcripts"]
-transcripts_dict= transcripts_list[0]
-
-# keys in subsection ['annotations', 'coding_end', 'coding_start', 'description', 'genomic_spans', 'length', 'reference', 'translation']
-# get the Refseq ID for database
-RefSeq = transcripts_dict["reference"]
-
-# bedfile header
-with open('output.bed', 'w') as f:
-    f.write("Chromosome\tstart\tend\tname\n")
-
-# get chromosome for BED
-annotations_dict = transcripts_dict["annotations"]
-chromosome = str(annotations_dict["chromosome"])
-
-# get start and end position for BED for each transcript
-genomic_spans_dict = transcripts_dict["genomic_spans"]
-for key in genomic_spans_dict:
-    temp_dict = genomic_spans_dict[key]
-    start = temp_dict["start_position"]
-    end = temp_dict["end_position"]
-
-    # for each transcript reference, add to bed file
-    with open('output.bed', 'a') as f:
-        f.write(chromosome + "\t" + str(start) + "\t" + str(end) + "\t" + str(key) + "\n")
+# HGNC will be called from raymond's section
+HGNC = ["1100", "4562"]
+call_transcript_make_bed(HGNC)
