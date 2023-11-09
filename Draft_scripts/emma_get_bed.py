78b22a9fe2befd52b6e8c78ba6d11b256a4af1d2
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2023-11-09 13:48:58 +0000

    basic variant validator API structure for RefSeq transcript, issue 20

--- Draft_scripts/emma_get_bed.py
+++ Draft_scripts/emma_get_bed.py
@@ -1,1 +1,16 @@
 # script for me to get bed file
+import requests
+
+
+# get HGNC id from JSON, API variant validator genes to transcripts, get bed files?
+# ie "HGNC:4562"
+# want the HGNC number
+# Merged into one bed for multiple gene beds?
+
+
+
+r = requests.get("https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A4562/mane_select/RefSeq/all", headers={ "content-type" : "application/json"})
+
+#returns data
+decoded = r.json()
+print(repr(decoded))
