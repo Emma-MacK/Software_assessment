34b961d215fce3be1dbb5686d80755faf6b5abfc
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2023-11-17 10:44:29 +0000

    Add flank argument to emma_get_bed.py function

--- Draft_scripts/emma_get_bed.py
+++ Draft_scripts/emma_get_bed.py
@@ -5,8 +5,9 @@ import pandas
 
 # ie "HGNC:4562"
 # want the HGNC number
+# add argument for flank value
 
-def call_transcript_make_bed(HGNC_list):
+def call_transcript_make_bed(HGNC_list, flank):
     url_base = "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A"
     transcript_filter = "/mane_select/refseq/GRCh37"
     for HGNC in HGNC_list:
@@ -51,8 +52,8 @@ def call_transcript_make_bed(HGNC_list):
             temp_dict = genomic_spans_dict[key]
             exon_list = temp_dict["exon_structure"]
             for item in exon_list:
-                start = item["genomic_start"]
-                end = item["genomic_end"]
+                start = int(item["genomic_start"]) - flank
+                end = int(item["genomic_end"]) + flank
                 exon = item["exon_number"]
                 # for each transcript reference, add to bed file
                 with open(filename, 'a') as f:
@@ -60,4 +61,5 @@ def call_transcript_make_bed(HGNC_list):
 
 # HGNC will be called from raymond's section
 HGNC = ["1100", "4562"]
-call_transcript_make_bed(HGNC)
+flank = 25
+call_transcript_make_bed(HGNC, flank)
