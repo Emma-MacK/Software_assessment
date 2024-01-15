6d9bf32f7cb3d13ad1ed60d1db68bd14b6c4b9a5
EKPayne <elena.payne@gstt.nhs.uk>
2024-01-15 16:01:10 +0000

    integrating logger into script

--- Draft_scripts/tool_v2.py
+++ Draft_scripts/tool_v2.py
@@ -1,8 +1,22 @@
 # read commandline inputs/user
 import argparse
+import logging
+import logging.config
+import logging.handlers as handlers
+import time
 import pandas as pd
 import requests
 from functions import get_target_ngtd, get_target_panelapp, check_testID
+
+# setting the logging.Formatter to use GMT time as default.
+# Guarantees that the log file always reflects UK local time
+logging.Formatter.converter = time.gmtime
+
+# loading logging configuration from the file
+logging.config.fileConfig('config/logging.conf')
+
+# create or get logger
+logger = logging.getLogger()
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-ID", "--testID", help="input the Test ID")
--- src/alternative_log.py
+++ src/alternative_log.py
@@ -3,11 +3,12 @@ import logging.config
 import logging.handlers as handlers
 import time
 
-# setting the logging.Formatter to use GMT time as default. This will guarantee that the log file reflects UK local time even when staff are abroad
+# setting the logging.Formatter to use GMT time as default.
+# Guarantees that the log file always reflects UK local time
 logging.Formatter.converter = time.gmtime
 
 # loading logging configuration from the file
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig('Draft_scripts/logging.conf') # TODO this file path will need to be updated once the file directory has been finalised
 
 # create or get logger
 logger = logging.getLogger()
@@ -17,4 +18,4 @@ logger.debug("Is it working?")
 logger.info("The logger is running")
 logger.warning("This will not print to console")
 logger.error("This will print to console")
-logger.critical("This will result in the programm not continuing")
+logger.critical("This will result in the program not continuing")
