cc43a1b73ede87f72a9fcfef5c9f233af971dfad
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2024-01-17 14:36:38 +0000

    make test: correct output files are made for a given input

--- Draft_scripts/make_bed_unit_test.py
+++ Draft_scripts/make_bed_unit_test.py
@@ -1,29 +1,34 @@
 # unit tests for function call_transcript_make_bed
-# when given set input, expext set output
+import unittest
+from unittest.mock import patch, mock_open
 
-from functions import all_transcript_make_bed
+from functions import call_transcript_make_bed
 
-
-#### entire function
 
 # given correct input, expected to make a bed file and a json file
+def test_expected_files():
+    # given the input HGNC 4562, expect files 4562_output.bed and 4562_VV_output.json
+    # if file names are changed, change tests
+    HGNC = [4562]
+    flank = 25
+    # set parameter so a file is not actually produced
+    mock_open_files = mock_open()
+    # anytiome open is called in functions, open mock files
+    with patch("functions.open", mock_open_files, create=True):
+        call_transcript_make_bed(HGNC, flank)
+    # check that while running call_transcript_make_bed, the files were interacted with
+    mock_open_files.assert_any_call("4562_output.bed", "w")
+    mock_open_files.assert_any_call("4562_VV_output.json", "w")
+
+# output files have expected content
+def test_file_content():
+
 # given incorrect input, expect informaticve error message
-
-
-#### api query
-
-# expect full url to be a string
-# expect when given a HGNC ID to be given a specific json file
+# output bedfile can be used by bedtools
 # if api does not connect, fail informatively
 # if HGNC id does not exist, fail informatively
-# if url is incorrect, fail informatively
-
-
-#### getting sub directories
-
+# if url customisation is incorrect, fail informatively
+# expect when given a HGNC ID to be given a specific json file
 # given specific input, expecting specific output
 
-#### building files
-
-# with specific input, expecting specific output
-
+# expected_files_test()
