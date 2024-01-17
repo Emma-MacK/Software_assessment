4fe23d2c3a558526450ce82b2177f8c49d7d7916
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2024-01-17 12:52:03 +0000

    make file for testing function call_transcript_make_bed, comment ideas for tests

---
+++ Draft_scripts/make_bed_unit_test.py
@@ -0,0 +1,29 @@
+# unit tests for function call_transcript_make_bed
+# when given set input, expext set output
+
+from functions import all_transcript_make_bed
+
+
+#### entire function
+
+# given correct input, expected to make a bed file and a json file
+# given incorrect input, expect informaticve error message
+
+
+#### api query
+
+# expect full url to be a string
+# expect when given a HGNC ID to be given a specific json file
+# if api does not connect, fail informatively
+# if HGNC id does not exist, fail informatively
+# if url is incorrect, fail informatively
+
+
+#### getting sub directories
+
+# given specific input, expecting specific output
+
+#### building files
+
+# with specific input, expecting specific output
+
