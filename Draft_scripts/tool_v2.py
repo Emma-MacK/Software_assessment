2e45660f835c5a382f811350e4a7420b5496535a
EKPayne <elena.payne@gstt.nhs.uk>
2024-01-21 19:03:54 +0000

    add additional commenting

--- Draft_scripts/tool_v2.py
+++ Draft_scripts/tool_v2.py
@@ -12,16 +12,21 @@ logging.Formatter.converter = time.gmtime
 # loading logging configuration from the file
 logging.config.fileConfig('config/logging.conf')
 
-# create or get logger
+# create/get logger
 logger = logging.getLogger()
 
+# creating an ArgumentParser to handle command-line arguments
+# adding -ID/--testID as an mandatory input
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-ID", "--testID", help="input the Test ID")
 
+# parsing the commandline argument input and saving it to variable testID
 args = argParser.parse_args()
 testID = args.testID
 
+# using check_testID function to confirm user input
 print(check_testID(testID))
 
+# pulling panel information from PanelApp Api
 genes_panelapp = get_target_panelapp(testID)
 print(genes_panelapp)
