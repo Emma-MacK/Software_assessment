645c326124b309b858270f84fe34a558c61e3588
EKPayne <elena.payne@gstt.nhs.uk>
2024-01-22 12:34:52 +0000

    Add initial logging statements
    Adding intital debug, info and warning logging. This required some changes to script to improve logging messages
    Additionally, at the same time, reformatted some (but not all) PEP8 violations.

--- Draft_scripts/functions.py
+++ Draft_scripts/functions.py
@@ -2,29 +2,42 @@
 # TO DO: add error handling
 import requests
 import json
-import pandas as pd
 import logging
+import logging.config
+import time
+import pandas as pd
 
-# Create or get the logger for the current module
-logger = logging.getLogger(__name__)
+# setting the logging.Formatter to use GMT time as default.
+# Guarantees that the log file always reflects UK local time
+logging.Formatter.converter = time.gmtime
+
+# loading logging configuration from logging.conf
+# logging will log to the same rotating file handler as tool_v2.py logging
+logging.config.fileConfig('config/logging.conf')
+
+# create/get logger
+logger = logging.getLogger()
 
 def get_target_ngtd(testID):
     """Get the genes in a panel from the NGTD for an testID
 
     Args:
-        testID (str): The ID of a specific test in the format Rxx
+        - testID (str): The ID of a specific test in the format Rxx
 
     Returns:
-        result_ngtd: A series (column from a pandas dataframe) of the target genes for a given test ID
+        - result_ngtd: A series (column from a pandas dataframe)
+        of the target genes for a given test ID
     """
 
-    # trun xlsx database into pandas dataframe
-    xls = 'Rare-and-inherited-disease-national-genomic-test-directory-version-5.1.xlsx'
+    # turn xlsx database into pandas dataframe
+    xls = ("Rare-and-inherited-disease-national-genomic-test-directory"
+           "-version-5.1.xlsx")
     test_directory_df = pd.read_excel(xls, 'R&ID indications', usecols="A:E", header=1)
-
     # get rows with a matching test code
     # does the testID exist in the test directory
-    panel = test_directory_df.loc[test_directory_df['Clinical indication ID'] == testID]
+    panel = test_directory_df.loc[
+        test_directory_df['Clinical indication ID'] == testID
+        ]
 
     # print columns
     result_ngtd = panel['Target/Genes'].to_string(index=False)
@@ -36,13 +49,14 @@ def get_target_panelapp(testID):
     """ Get panel information from panel app for the given test ID
 
     Args:
-        testID (str): The ID of a specific test in the format Rxx
+        - testID (str): The ID of a specific test in the format Rxx
 
     Raises:
-        SystemExit: Unable to connect to panel app
+        - SystemExit: Unable to connect to panel app
 
     Returns:
-        result_panelapp: The json file returned from querying panelapp, holds information for the given R code
+        - result_panelapp: The json file returned from querying panelapp,
+        holds information for the given R code
     """
 
     server = "https://panelapp.genomicsengland.co.uk/api/v1"
@@ -51,27 +65,30 @@ def get_target_panelapp(testID):
 
     # use try and except to handle errors connecting to panelapp
     try:
-        r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
+        logging.info("Attempting to pull data from PanelApp Api")
+        r = requests.get(server+ext, headers={"Content-Type" : "application/json"}) # TODO Sentence to long for PEP8
         decoded = r.json()
-        result_panelapp = repr(decoded)
+        result_panelapp = json.dumps(decoded, indent = 2)
         return result_panelapp
 
     except requests.exceptions.RequestException as e:
             # get details of exception
         print("An exception occurred connecting to PanelApp")
-        raise SystemExit(e)
-        exit()
+        logging.error("Unable to connect to Panel App. System Exit raised") # TODO Sentence to long for PEP8
+        raise SystemExit(e) from e
+        # exit()
 
 def check_testID(testID):
     """Test the given R code to ensure it is in the correct format
       and check it exists in the test directory
 
     Args:
-        testID (str): The ID of a specific test in the format Rxx
+        - testID (str): The ID of a specific test in the format Rxx
 
     Returns:
-        result (str): A printed string informing user of an incorrect format,
-        or prompt the user to continue if the R code does not exist in the test directory
+        - result (str): A printed string informing user of an incorrect
+        format, or prompt the user to continue if the R code does not
+        exist in the test directory
     """
     in_test_directory=get_target_ngtd(testID)
     # check the ID begins with R
@@ -80,21 +97,24 @@ def check_testID(testID):
     # elif does not exist in the test directory
     else:
         if in_test_directory == "Series([], )":
-            print("Test ID not found within the national genomic test directory")
+            print("Test ID not found within the test directory")
+            logging.info("Test ID not found within the test directory")
             # ask user if they want to continue
             want_continue = "Please enter y or n"
             while want_continue == "Please enter y or n":
                 want_continue = input("Do you want to continue with a code not in the test directory? [y/n]: ")
                 if want_continue == "y":
-                    result = f"Proceeding with test ID {testID} anyway"
+                    result = f"Proceeding with test ID {testID}"
+                    logging.warning("User proceeding with non-NGTD panel")
                     print(result)
                 elif want_continue == "n":
+                    logging.info("User requested system exit")
                     exit()
                 else:
                     want_continue = "Please enter y or n"
                     print(want_continue)
         else:
-            result = "R code found in test directory"
+            result = "Test ID found in the national genomic test directory"
     return result
 
 
@@ -105,16 +125,20 @@ def call_transcript_make_bed(HGNC_list, flank, genome_build,
     Builds a bed file and a json file for use in our database
 
     Args:
-        HGNC_list (str): A gene ID to query in variant validator
-        flank (int): The number of bases add as a flank to regions in bed file
-        genome_build (str): The genome build to use. GRCh37, GRCh38, or all
-        transcript_set (str): the transcript source to use. refseq, ensembl, or all
-        limited_transcripts (str): The transcripts to use. mane_select, mane, select
+        - HGNC_list (str): A gene ID to query in variant validator
+        flank (int): The number of bases add as a flank to regions in
+        bed file
+        - genome_build (str): The genome build to use. GRCh37, GRCh38,
+        or all
+        - transcript_set (str): the transcript source to use. refseq,
+        ensembl, or all
+        - limited_transcripts (str): The transcript to use. mane_select,
+        mane, select
     """
 
     # build the url for connecting to variant validator
-    url_base = "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A"
-    transcript_filter = "/" + limited_transcripts + "/" + transcript_set + "/" + genome_build
+    url_base = ("https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A/") # TODO to long for PEP8 standards
+    transcript_filter = "/" + limited_transcripts + "/" + transcript_set + "/" + genome_build # TODO to long for PEP8 standards
 
     # make empty bed file
     print("Making bed file for gene list:" + str(HGNC_list))
@@ -126,24 +150,30 @@ def call_transcript_make_bed(HGNC_list, flank, genome_build,
     for HGNC in HGNC_list:
         full_url = url_base + str(HGNC)+ transcript_filter
         print("querying: " + full_url)
+        logging.info("Querying: %s", full_url)
         try:
-            r = requests.get(full_url, headers={ "content-type" : "application/json"})
+            r = requests.get(full_url, headers={ "content-type" : "application/json"}) # TODO to long for PEP8 standards. 
             decoded = r.json()
-            # print(repr(decoded))
+            print(repr(decoded))
             json_dict = decoded[0]
-        except:
+        except requests.exceptions.RequestException as e:
             print("An exception occurred connecting to variant validator")
-            exit()
+            logging.error("Unable to connect to VariantValidator,"
+                          " System exit raised")
+            raise SystemExit(e) from e
+            # exit()
 
         # if the gene symbol does not exist returns {'error': 'Unable to recognise gene symbol NO DATA', 'requested_symbol': 'NO DATA'}
         # if json_dict["error"] exists print the error and exit
         if 'error' in json_dict:
-            print("An error occured with variant validator: No data available for this request")
+            print("An error occured connecting to variant validator: "
+                  "No data available for this request")
             print(json_dict['error'])
             exit()
 
         # Keys in json ['current_name', 'current_symbol', 'hgnc', 'previous_symbol', 'requested_symbol', 'transcripts']
-        print("JSON found")
+        # print("JSON found")
+        logging.info("Json successfully returned")
         transcripts_list = json_dict["transcripts"]
 
         # case where entry (using genome_build = "GRCh38", transcript_set = "ensembl", limited_transcripts = "select")
@@ -152,10 +182,13 @@ def call_transcript_make_bed(HGNC_list, flank, genome_build,
 
         if not transcripts_list:
             print(json_dict)
-            print("No transcript information available, cannot produce bed file for this request")
+            print("No transcript information available, cannot produce bed "
+                  "file for this request")
+            logging.error("Transcript information unavailable. "
+                          "Unable to produce bed file. System exit raised")
             exit()
 
-        transcripts_dict= transcripts_list[0]
+        transcripts_dict = transcripts_list[0]
 
 
         # keys in subsection ['annotations', 'coding_end', 'coding_start', 'description', 'genomic_spans', 'length', 'reference', 'translation']
@@ -180,6 +213,7 @@ def call_transcript_make_bed(HGNC_list, flank, genome_build,
         # Writing to sample.json
         with open(json_name, "w") as outfile:
             outfile.write(json_object)
+        logging.info("json containing data for database entry created")
 
         # make bedfile header
         print("Making bed file for HGNC:" + str(HGNC))
@@ -194,6 +228,7 @@ def call_transcript_make_bed(HGNC_list, flank, genome_build,
         for key in genomic_spans_dict:
             temp_dict = genomic_spans_dict[key]
             exon_list = temp_dict["exon_structure"]
+            logging.debug("")
 
         # get strand for bed, held in orientation. 1 = +, -1 = -
             if temp_dict["orientation"] == 1:
@@ -212,3 +247,4 @@ def call_transcript_make_bed(HGNC_list, flank, genome_build,
                 # add to big bed file
                 with open(concat_filename, 'a') as f:
                     f.write(chromosome + "\t" + str(start) + "\t" + str(end) + "\t" + label + "\t" + str(0) +"\t" + sense + "\n")
+        logging.info("Bedfiles created. %sbp flanking region used", flank)
--- Draft_scripts/tool_v2.py
+++ Draft_scripts/tool_v2.py
@@ -1,5 +1,6 @@
 # read commandline inputs/user
 import argparse
+import json
 import logging
 import logging.config
 import time
@@ -18,15 +19,22 @@ logger = logging.getLogger()
 # creating an ArgumentParser to handle command-line arguments
 # adding -ID/--testID as an mandatory input
 argParser = argparse.ArgumentParser()
-argParser.add_argument("-ID", "--testID", help="input the Test ID")
+argParser.add_argument("-ID", "--testID", help="input the Test ID", required=True)
 
 # parsing the commandline argument input and saving it to variable testID
 args = argParser.parse_args()
 testID = args.testID
+logging.info("Request received for %s panel", testID)
 
 # using check_testID function to confirm user input
-print(check_testID(testID))
+confirmed_testid = check_testID(testID)
+logging.info(confirmed_testid)
 
 # pulling panel information from PanelApp Api
 genes_panelapp = get_target_panelapp(testID)
-print(genes_panelapp)
+
+# Loading json into a dictionary and logging panel name
+genes_panelapp_dict = json.loads(genes_panelapp)
+panel_name = genes_panelapp_dict.get("name")
+print(f"Panel information pulled for {testID}:{panel_name}")
+logging.info("Successfully pulled data for %s:%s", testID, panel_name)
