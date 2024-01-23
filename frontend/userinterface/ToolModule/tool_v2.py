04aa12806b1b80def7e2cde1e3797777c481f110
raymondmiles <raymond.c.miles@gmail.com>
2024-01-23 16:37:44 +0000

    Added in comments and removed unnecessary comments

--- frontend/userinterface/ToolModule/tool_v2.py
+++ frontend/userinterface/ToolModule/tool_v2.py
@@ -6,15 +6,14 @@ import logging.handlers as handlers
 import time
 import pandas as pd
 import requests
-from ToolModule.functions import get_target_ngtd, get_target_panelapp, check_testID
+from functions import check_testID,get_target_panelapp,get_target_ngtd,call_transcript_make_bed
 
 # setting the logging.Formatter to use GMT time as default.
 # Guarantees that the log file always reflects UK local time
 logging.Formatter.converter = time.gmtime
 
 # loading logging configuration from the file
-logging.config.fileConfig('config/logging.conf') #Check where to take it in from
-
+logging.config.fileConfig('../../../config/logging.conf') #Added in relative path
 # create or get logger
 logger = logging.getLogger()
 
